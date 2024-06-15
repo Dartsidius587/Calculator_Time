39550eda90ca495ff178128a12e88ec7a3e59858
Papa_PC <dartsidius156@gmail.com>
2024-06-15 16:38:42 +0300

    1. Добавлен toolbar 2. Внесены изменения согласно задания 3. Добавлено меню 4. Выполнено задание со звездой

--- app/src/main/java/com/example/calculator_time/MainActivity.kt
+++ app/src/main/java/com/example/calculator_time/MainActivity.kt
@@ -2,24 +2,25 @@ package com.example.calculator_time
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var toolbarMain: androidx.appcompat.widget.Toolbar
 
     private lateinit var firstOperandET: EditText
     private lateinit var secondOperandET: EditText
 
     private lateinit var buttonSumBTN: Button
     private lateinit var buttonDifBTN: Button
-    private lateinit var buttonResetBTN: Button
-    private lateinit var buttonExitBTN: Button
 
     private lateinit var resultTV: TextView
 
@@ -33,41 +34,84 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     fun init(){
         setContentView(R.layout.activity_main)
 
+        toolbarMain = findViewById(R.id.toolbarMain)
+        setSupportActionBar(toolbarMain)
+        title = "Калькулятор времени"
+        toolbarMain.subtitle = "версия 1.1"
+        toolbarMain.setLogo(R.drawable.calck_time)
+
         firstOperandET = findViewById(R.id.firstOperandET)
         secondOperandET = findViewById(R.id.secondOperandET)
 
         buttonSumBTN = findViewById(R.id.buttonSumBTN)
         buttonDifBTN = findViewById(R.id.buttonDifBTN)
-        buttonResetBTN = findViewById(R.id.buttonResetBTN)
-        buttonExitBTN = findViewById(R.id.buttonExitBTN)
 
         resultTV = findViewById(R.id.resultTV)
 
         buttonSumBTN.setOnClickListener(this)
         buttonDifBTN.setOnClickListener(this)
-        buttonResetBTN.setOnClickListener(this)
-        buttonExitBTN.setOnClickListener(this)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.resetMenuMain -> {
+                firstOperandET.text.clear()
+                secondOperandET.text.clear()
+                resultTV.text = ""
+                Toast.makeText(
+                    applicationContext,
+                    "Данные очищены",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+            R.id.exitMenuMain -> {
+                Toast.makeText(
+                    applicationContext,
+                    "Приложение закрыто",
+                    Toast.LENGTH_LONG
+                ).show()
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onClick(v: View) {
-        var check = true
+
         if(firstOperandET.text.isEmpty() || secondOperandET.text.isEmpty()) return
 
-        var first = firstOperandET.text.toString()
-        var second = secondOperandET.text.toString()
-
-        var result = when(v.id){
-            R.id.buttonSumBTN -> Operation(first, second).sum()
-            R.id.buttonDifBTN -> Operation(first, second).dif()
-            R.id.buttonResetBTN -> {
-                firstOperandET.text.clear()
-                secondOperandET.text.clear()
-                check = false
+        val first = firstOperandET.text.toString()
+        val second = secondOperandET.text.toString()
+        var result = ""
+        when(v.id){
+            R.id.buttonSumBTN -> {
+                result = Operation(first, second).sum()
+                Toast.makeText(
+                    applicationContext,
+                    "Результат: ${Operation(first, second).sum()}",
+                    Toast.LENGTH_LONG
+                ).show()
             }
-            R.id.buttonExitBTN -> finish()
+
+            R.id.buttonDifBTN -> {
+                result = Operation(first, second).dif()
+                Toast.makeText(
+                    applicationContext,
+                    "Результат: ${Operation(first, second).dif()}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
             else -> "Error"
         }
-        if(!check) resultTV.text = "Результат" else resultTV.text = result.toString()
-    }
 
+        resultTV.text = result
+    }
 }
